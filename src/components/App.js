0d9a0efb7359a0d752358f5a0bd4ae6fa0780b52
joao-vitor <joaovitorfelixcontato@gmail.com>
2022-10-17 20:41:29 -0300

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="container">
         <Banner />
         <Searchbox search={search} setSearch={setSearch} />
